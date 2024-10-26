768435f336248af7034d4acfd1ec6b65222b37a8
mdah <mda.hewlett@gmail.com>
2024-10-26 11:39:51 -0700

    feat: include header section num, ignore first 20 edge case pages, ensure consistent page summary dict fields

--- src/make_page_summaries.py
+++ src/make_page_summaries.py
@@ -11,12 +11,16 @@ from typing import List
 import io
 import re
 
+
 # Defining structure of the page summary text
 class PageSummary(BaseModel):
     page_num: int
     section_num: str
 
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
+)
 
 load_dotenv()
 
@@ -28,29 +32,28 @@ aws_region = os.getenv("AWS_REGION", "us-east-1")
 logging.info("Initializing OpenAI client")
 client = OpenAI(api_key=openai_api_key)
 textract = boto3.client(
-    'textract',
+    "textract",
     aws_access_key_id=aws_access_key_id,
     aws_secret_access_key=aws_secret_access_key,
-    region_name=aws_region
+    region_name=aws_region,
 )
 
+
 def load_coordinate_zones(json_path):
-    with open(json_path, 'r') as f:
+    with open(json_path, "r") as f:
         return json.load(f)
 
+
 def extract_text_from_zones(image, zones):
-    zone_texts = {
-        'page_header': [],
-        'page_footer': []
-    }
+    zone_texts = {"page_header": [], "page_footer": []}
 
     for zone_name, zone_coords in zones.items():
-        
+
         coordinates = (
-            zone_coords['left'],
-            zone_coords['top'],
-            zone_coords['right'],
-            zone_coords['bottom']
+            zone_coords["left"],
+            zone_coords["top"],
+            zone_coords["right"],
+            zone_coords["bottom"],
         )
 
         # crop the image to the zone of interest
@@ -58,23 +61,19 @@ def extract_text_from_zones(image, zones):
 
         # convert image format
         img_byte_arr = io.BytesIO()
-        zone.save(img_byte_arr, format='PNG')
+        zone.save(img_byte_arr, format="PNG")
         img_byte_arr = img_byte_arr.getvalue()
 
         # call API to extract text
-        response = textract.detect_document_text(
-            Document={'Bytes': img_byte_arr}
-        )
+        response = textract.detect_document_text(Document={"Bytes": img_byte_arr})
 
         # sort text into header/footer to separate section name from topics and tags
-        for item in response['Blocks']:
-            if item['BlockType'] == 'LINE':
-                zone_texts[zone_name].append(item['Text'])
-    
-    logging.info(f"Page header text:\n{zone_texts['page_header']}")
-    logging.info(f"Page footer text:\n{zone_texts['page_footer']}")
-        
-    return zone_texts['page_header'], zone_texts['page_footer']
+        for item in response["Blocks"]:
+            if item["BlockType"] == "LINE":
+                zone_texts[zone_name].append(item["Text"])
+
+    return zone_texts["page_header"], zone_texts["page_footer"]
+
 
 def extract_text_from_pages(pdf_path, zones):
 
@@ -87,45 +86,61 @@ def extract_text_from_pages(pdf_path, zones):
         # give developper progress updates
         if i != 0 and i % 10 == 0:
             logging.info(f"extracting text from page number {i}")
-        
-        page_dict = {'topic_list': []}
+
+        page_dict = {
+            "section_num": "",
+            "section_name": "",
+            "section_num_detailed": "",
+            "page_num": i,
+            "topic_list": [],
+        }
+
+        # ignore first 20 pages as edge cases for now
+        if i < 19:
+            results.append(page_dict)
+            continue
 
         # extract text
-        page_header, page_footer = extract_text_from_zones(page, zones) 
-        
-        # add section name from header text, throw out high level section number
+        page_header, page_footer = extract_text_from_zones(page, zones)
+
+        # add section name and number from header text
         for item in page_header:
+            item = item.strip()
             if re.match(r"^\d+$", item):
-                continue
+                page_dict["section_num"] = item
             else:
-                page_dict['section_name'] = item
-        
+                page_dict["section_name"] = item
+
         # add remaining page info from footer text
         for item in page_footer:
+            item = item.strip()
             # section number
             if re.match(r"^\d+(\.\d+)?[A-Za-z]?$", item):
-                page_dict['section_num'] = item
-            
+                page_dict["section_num_detailed"] = item
+
             # throw out page format e.g. A-10, G-4
             elif re.match(r"^[A-Za-z]-\d+$", item):
                 continue
-            
+
             # topics and tags go together
             else:
-                page_dict['topic_list'].append(item)
-        
-        # add page number
-        page_dict['page_num'] = i
+                page_dict["topic_list"].append(item)
 
         results.append(page_dict)
     return results
+
 
 def main():
     current_dir = os.path.dirname(os.path.abspath(__file__))
 
     # select manual pdfs and coordinate zones to test
-    pdf_path = os.path.join(current_dir, '..', 'data', 'raw', 'ocr_30page.pdf')
-    json_path = os.path.join(current_dir, '..', 'data', 'processed', 'ocr_coordinates.json')
+    pdf_path = os.path.join(current_dir, "..", "data", "raw", "ocr_130page.pdf")
+    json_path = os.path.join(
+        current_dir, "..", "data", "processed", "ocr_coordinates.json"
+    )
+    save_filepath = os.path.join(
+        current_dir, "..", "data", "processed", "test1_page_summaries.json"
+    )
 
     zones = load_coordinate_zones(json_path)
     logging.info(f"Loaded coordinate zones: {zones}")
@@ -134,6 +149,14 @@ def main():
     logging.info(f"Results:\n")
     for result in results:
         logging.info(f"{result}\n")
+
+    logging.info(f"Format of results object:\n{results}")
+
+    logging.info(f"Saving json page summaries")
+    with open(save_filepath, "w") as f:
+        json.dump(results, f)
+    logging.info(f"Saved json page summaries")
+
 
 if __name__ == "__main__":
     main()
